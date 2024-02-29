dd02ad712de1bdd3fc992b439d9b0f2725b54fd8
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2024-02-29 17:08:19 +0100

    feat: add GET handler and check for CRON_SECRET in headers

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  return Response.json({ success: true });
+}
