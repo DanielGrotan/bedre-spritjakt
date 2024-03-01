4e9122e7ff8b2ae880fbadcfa1b63261ed82e760
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2024-03-01 16:27:11 +0100

    feat: add indices to prices table and createdAt field to products table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,7 @@
 import {
   doublePrecision,
+  index,
   pgTable,
-  serial,
   text,
   timestamp,
 } from "drizzle-orm/pg-core";
@@ -11,13 +11,27 @@ export const products = pgTable("products", {
   abv: doublePrecision("abv").notNull(),
   volume: doublePrecision("volume").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
 });
 
-export const prices = pgTable("prices", {
-  id: serial("id").primaryKey(),
-  productId: text("product_id").notNull(),
-  store: text("store").notNull(),
-  price: doublePrecision("price").notNull(),
-  alcoholUnitPrice: doublePrecision("alcohol_unit_price").notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-});
+export const prices = pgTable(
+  "prices",
+  {
+    productId: text("product_id")
+      .notNull()
+      .references(() => products.id),
+    store: text("store").notNull(),
+    price: doublePrecision("price").notNull(),
+    alcoholUnitPrice: doublePrecision("alcohol_unit_price").notNull(),
+    timestamp: timestamp("timestamp").defaultNow(),
+  },
+  (table) => ({
+    productIdIdx: index("product_id_idx").on(table.productId),
+    storeIdx: index("store_idx").on(table.store),
+    priceIdx: index("price_idx").on(table.price),
+    alcoholUnitPriceIdx: index("alcohol_unit_price_idx").on(
+      table.alcoholUnitPrice
+    ),
+    timestampIdx: index("timestamp_idx").on(table.timestamp),
+  })
+);
