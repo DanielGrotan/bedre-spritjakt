4a071646e1a3aaee025cf5382188b827309c081d
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2024-03-01 15:40:40 +0100

    feat: separate data into different tables

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,11 +1,23 @@
-import { doublePrecision, pgTable, serial, text } from "drizzle-orm/pg-core";
+import {
+  doublePrecision,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
 export const products = pgTable("products", {
-  id: serial("id").primaryKey(),
-  externalId: text("external_id").notNull(),
-  store: text("store").notNull(),
+  id: text("id").notNull().primaryKey(),
   abv: doublePrecision("abv").notNull(),
   volume: doublePrecision("volume").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+});
+
+export const prices = pgTable("prices", {
+  id: serial("id").primaryKey(),
+  productId: text("product_id").notNull(),
+  store: text("store").notNull(),
   price: doublePrecision("price").notNull(),
   alcoholUnitPrice: doublePrecision("alcohol_unit_price").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
 });
